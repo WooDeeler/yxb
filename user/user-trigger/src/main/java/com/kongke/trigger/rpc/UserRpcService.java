64f8b88a8ec3b7288fb66dabd2d63053795ba607
kongke <kongke27@qq.com>
2025-04-15 01:09:43 +0800

    feat: docker

--- user/user-trigger/src/main/java/com/kongke/trigger/rpc/UserRpcService.java
+++ user/user-trigger/src/main/java/com/kongke/trigger/rpc/UserRpcService.java
@@ -6,6 +6,7 @@ import com.kongke.domain.userlogin.model.vo.UserVO;
 import com.kongke.domain.userlogin.service.UserService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * rpc实现
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author kongke
  * @date 2025/04/07
  */
+@Service
 @DubboService
 public class UserRpcService implements IUserService {
 
